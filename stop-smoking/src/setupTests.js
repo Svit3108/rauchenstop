f7b83a7ac7c3f7127f7b2125cb3a859bc37f60ca
Svitlana Popova <diduhdiduh367@gmail.com>
2024-07-30 00:05:40 +0200

    erstelen eine schöne test

--- stop-smoking/src/setupTests.js
+++ stop-smoking/src/setupTests.js
@@ -1,5 +1,12 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// Importiere die erforderlichen Test-Utils
+import '@testing-library/jest-dom/extend-expect';
+
+// Optionale zusätzliche Setups, wie Mocking von APIs oder globalen Stilen
+// Hier ist ein Beispiel für das Setzen eines globalen Mock für das Fetch API
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+  })
+);
+
+// Optional: Setup für Testlaufzeit-Utilities oder globale Konfigurationen
